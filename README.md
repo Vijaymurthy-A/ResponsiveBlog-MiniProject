dba724894986c4cc4e510a934381aad13af2f87a
Vijaymurthy-A <vijaymurthy95.a@gmail.com>
2023-11-14 20:41:00 +0530

    Created a minimalistic and responsive blog

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# ResponsiveBlog-MiniProject
+# Responsive Blog
+
+## Using plain HTML, CSS, JavaScript
+
+Simple Responsive blog page that lists static contents.
+
+Hosted on [netlify]()
+
+## Features
+
+- Used a search bar to quicker results
+- First page lists clean and minimalistic user interface for the blog.
+- Second page uses client side navigation that dynamically changes content without any reloads
+- Link to the home page using the title name in the nav bar
---
+++ scripts/script.js
@@ -0,0 +1,73 @@
+console.log("I'm from script.js");
+
+// Used sessionStorage to keep track on which title was clicked by the user
+// const nodes = document.querySelectorAll("#title");
+// for (const node of nodes) {
+//   node.addEventListener("click", function (event) {
+//     sessionStorage.setItem("displayBlog", event.target.title);
+//   });
+// }
+
+function navigateToSection(sectionId, event) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    event.preventDefault();
+    console.log(section.offsetTop);
+    // Scroll to the section with an animated behavior
+    window.scrollTo({
+      top: section.offsetTop,
+    });
+  }
+}
+
+let topics = new Map();
+document
+  .querySelectorAll("#title > h3")
+  .forEach((element) => topics.set(element.innerText, element.title));
+
+if (topics.size == 0) {
+  document
+    .querySelectorAll("#title > h1")
+    .forEach((element) => topics.set(element.innerText, element.title));
+}
+function searchQuery() {
+  let results = new Map();
+  const searchQuery = document.getElementById("search").value;
+  if (searchQuery !== "") {
+    topics.forEach((value, topic) => {
+      if (topic.toUpperCase().includes(searchQuery.toUpperCase())) {
+        results.set(topic, value);
+      }
+    });
+  }
+
+  let popupElement = document.getElementById("searchPopup");
+  while (popupElement.hasChildNodes()) {
+    popupElement.removeChild(popupElement.firstChild);
+  }
+
+  if (results.size > 0) {
+    results.forEach((value, key) => {
+      const div = document.createElement("a");
+      div.setAttribute("href", `./blogs.html#${value}`);
+      div.setAttribute("onclick", `navigateToSection("${value}", event)`);
+      div.innerText = key;
+      popupElement.appendChild(div);
+    });
+  }
+}
+
+document.addEventListener("click", (event) => {
+  let searchDiv = document.getElementById("searchDiv");
+  let popupDiv = document.getElementById("searchPopup");
+
+  if (
+    popupDiv.hasChildNodes() &&
+    event.target !== searchDiv &&
+    event.target !== popupDiv
+  ) {
+    while (popupDiv.hasChildNodes()) {
+      popupDiv.removeChild(popupDiv.firstChild);
+    }
+  }
+});
